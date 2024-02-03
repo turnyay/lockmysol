8bfff03500bd4339eaf08bfc7d45cd6de7d754eb
Mike TOurigny <mtourig3@gmail.com>
2024-02-03 18:26:10 -0500

    added 4 instructions and 4 tests

--- program/lockmysol/tests/lockmysol.ts
+++ program/lockmysol/tests/lockmysol.ts
@@ -8,9 +8,23 @@ describe("lockmysol", () => {
 
   const program = anchor.workspace.Lockmysol as Program<Lockmysol>;
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
+  it("Is able to lock solana", async () => {
+    const tx = await program.methods.lockSolForTime().rpc();
+    console.log("Locking SOL txid: ", tx);
+  });
+
+  it("Is able to unlock solana", async () => {
+    const tx = await program.methods.unlockSol().rpc();
+    console.log("Unlocking SOL txid: ", tx);
+  });
+
+  it("Is able to lock tokens", async () => {
+    const tx = await program.methods.lockTokensForTime().rpc();
+    console.log("Locking Tokens txid: ", tx);
+  });
+
+  it("Is able to lock solana", async () => {
+    const tx = await program.methods.unlockTokens().rpc();
+    console.log("Locking SOL txid: ", tx);
   });
 });
