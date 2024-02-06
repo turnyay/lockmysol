ff7cc1870aaa48e938712fec9a4f1917753cc9d6
Mike TOurigny <mtourig3@gmail.com>
2024-02-05 19:59:01 -0500

    added ui updates lock solana

--- client/src/components/AppBar.tsx
+++ client/src/components/AppBar.tsx
@@ -55,13 +55,13 @@ export const AppBar: React.FC = () => {
         <div className="navbar-end">
           <div className="hidden md:inline-flex align-items-center justify-items gap-6">
           <NavElement
-            label="Home"
+            label="Lock"
             href="/"
             navigationStarts={() => setIsNavOpen(false)}
           />
           <NavElement
-            label="Basics"
-            href="/basics"
+            label="My Locks"
+            href="/my-locks"
             navigationStarts={() => setIsNavOpen(false)}
           />
           <WalletMultiButtonDynamic className="btn-ghost btn-sm rounded-btn text-lg mr-6 " />
