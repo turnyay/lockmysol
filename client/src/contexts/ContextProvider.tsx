adcf65cb643b04a92908c84d717ab69664328a5a
Mike TOurigny <mtourig3@gmail.com>
2024-02-14 23:59:21 -0500

    context walletprovider

--- client/src/contexts/ContextProvider.tsx
+++ client/src/contexts/ContextProvider.tsx
@@ -1,7 +1,9 @@
 import { WalletAdapterNetwork, WalletError } from '@solana/wallet-adapter-base';
 import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react';
 import {
-    UnsafeBurnerWalletAdapter
+    BackpackWalletAdapter,
+    PhantomWalletAdapter,
+    SolflareWalletAdapter,
 } from '@solana/wallet-adapter-wallets';
 import { Cluster, clusterApiUrl } from '@solana/web3.js';
 import { FC, ReactNode, useCallback, useMemo } from 'react';
@@ -11,24 +13,33 @@ import { NetworkConfigurationProvider, useNetworkConfiguration } from './Network
 import dynamic from "next/dynamic";
 
 const ReactUIWalletModalProviderDynamic = dynamic(
-  async () =>
-    (await import("@solana/wallet-adapter-react-ui")).WalletModalProvider,
-  { ssr: false }
+    async () =>
+        (await import("@solana/wallet-adapter-react-ui")).WalletModalProvider,
+    { ssr: false }
 );
 
 const WalletContextProvider: FC<{ children: ReactNode }> = ({ children }) => {
     const { autoConnect } = useAutoConnect();
     const { networkConfiguration } = useNetworkConfiguration();
     const network = networkConfiguration as WalletAdapterNetwork;
-    const endpoint = useMemo(() => clusterApiUrl(network), [network]);
+    let endpoint = useMemo(() => clusterApiUrl(network), [network]);
 
-    console.log(network);
+    if (network === 'devnet') {
+        endpoint = useMemo(() => "http://api.devnet.solana.com", [network]);
+        // endpoint = useMemo(() => "http://127.0.0.1:8899", [network]);
+        // endpoint = useMemo(() => "https://rpc-devnet.hellomoon.io/a292109c-429c-4e87-9567-c29ae9743ecd", [network]);
+        // endpoint = useMemo(() => "https://rpc-devnet.helius.xyz/?api-key=fbe23b51-5eb3-4b36-bc0c-78b129a8757e", [network]);
+    }
+
+    // console.log(network);
+    // console.log(endpoint);
 
     const wallets = useMemo(
         () => [
-            new UnsafeBurnerWalletAdapter(),
-        ],
-        [network]
+            // new BackpackWalletAdapter(),  // autodetect
+            // new PhantomWalletAdapter(),   // autodetect
+            new SolflareWalletAdapter()
+        ], [network]
     );
 
     const onError = useCallback(
@@ -46,7 +57,7 @@ const WalletContextProvider: FC<{ children: ReactNode }> = ({ children }) => {
                 <ReactUIWalletModalProviderDynamic>
                     {children}
                 </ReactUIWalletModalProviderDynamic>
-			</WalletProvider>
+            </WalletProvider>
         </ConnectionProvider>
     );
 };
