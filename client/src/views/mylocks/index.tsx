7b7a6476a473c4f83a3f25357133e70e61bd46af
Mike TOurigny <mtourig3@gmail.com>
2024-02-16 19:28:22 -0500

    able to unlock solana from my locks tab

--- client/src/views/mylocks/index.tsx
+++ client/src/views/mylocks/index.tsx
@@ -27,6 +27,8 @@ const Input = styled(MuiInput)`
   width: 42px;
 `;
 
+import { notify } from "../../utils/notifications";
+
 // Wallet
 import { useWallet, useConnection } from '@solana/wallet-adapter-react';
 
@@ -109,6 +111,38 @@ export const MylocksView: FC = ({ }) => {
     fetchData();
   }, [wallet.publicKey, connection, getUserSOLBalance, setInitialized])
 
+  const handleUnlockButtonClick  = async (lockNumber) => {
+    console.log(`Unlocking SOLANA for lock ID: `, lockNumber);
+
+    const provider = new anchor.AnchorProvider(
+        connection,
+        wallet,
+        {
+            commitment: "recent",
+            preflightCommitment: "recent",
+            skipPreflight: true,
+        }
+    );
+    const program = new Program(IDL, programId, provider);
+
+    const lockmysol = new LockmysolProgram({
+      provider: provider,
+      programId: programId,
+      program: program,
+    });
+
+    try {
+      const result = await lockmysol.unlockSol(lockNumber);
+      if (result) {
+        notify({ type: 'success', message: 'Transaction successful!' });
+      } else {
+        notify({ type: 'error', message: 'Transaction error!' });
+      }
+    } catch (e) {
+      
+    }
+  };
+
   return (
     <div className="md:hero mx-auto p-4">
       <div className="md:hero-content flex flex-col">
@@ -120,22 +154,26 @@ export const MylocksView: FC = ({ }) => {
             <TableHead>
               <TableRow>
                 <TableCell sx={{ color: 'white' }}>Lock Number</TableCell>
-                <TableCell sx={{ color: 'white' }}>Owner</TableCell>
+                <TableCell sx={{ color: 'white' }}>Asset</TableCell>
                 <TableCell sx={{ color: 'white' }}>Amount</TableCell>
                 <TableCell sx={{ color: 'white' }}>State</TableCell>
                 <TableCell sx={{ color: 'white' }}>Unlock Time</TableCell>
                 <TableCell sx={{ color: 'white' }}>Unlocking In</TableCell>
+                <TableCell sx={{ color: 'white' }}>Unlock</TableCell>
               </TableRow>
             </TableHead>
             <TableBody>
               {lockAccounts.map((lockAccount) => (
                 <TableRow key={lockAccount.lockNumber}>
                   <TableCell sx={{ color: 'white' }}>{lockAccount.lockNumber}</TableCell>
-                  <TableCell sx={{ color: 'white' }}>{lockAccount.owner}</TableCell>
+                  <TableCell sx={{ color: 'white' }}>{"SOL"}</TableCell>
                   <TableCell sx={{ color: 'white' }}>{lockAccount.amount / 1000000000}</TableCell>
                   <TableCell sx={{ color: 'white' }}>{lockAccount.state == 1 ? "Locked" : "Complete"}</TableCell>
                   <TableCell sx={{ color: 'white' }}>{lockAccount.unlockTime}</TableCell>
                   <TableCell sx={{ color: 'white' }}>{lockAccount.unlockingIn} seconds</TableCell>
+                  <TableCell sx={{ color: 'white' }}>
+                    <Button variant="contained" onClick={() => handleUnlockButtonClick(lockAccount.lockNumber)}>UNLOCK</Button>
+                  </TableCell>
                 </TableRow>
               ))}
             </TableBody>
